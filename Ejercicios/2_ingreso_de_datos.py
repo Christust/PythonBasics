0296ba0904991b424e24a3490497d4803b09e991
Christos Marroquin <christos.marroquin@hotmail.com>
2024-03-21 15:58:36 -0600

    Clase 2

--- Ejercicios/2_ingreso_de_datos.py
+++ Ejercicios/2_ingreso_de_datos.py
@@ -1,5 +1,4 @@
 lado = input("Ingrese la medida uno de los lados: ")
 lado = int(lado)
 superficie = lado * lado
-print("La superficie del cuadrado es")
-print(superficie)
+print(f"La superficie del cuadrado es {superficie}")
